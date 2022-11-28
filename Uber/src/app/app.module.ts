314d7f6f2efa113c2fd31439d92a50cd5f3922ff
savic-a <anastasijas557@gmail.com>
2022-11-28 01:30:48 +0100

    Fixing bugs

--- Uber/src/app/app.module.ts
+++ Uber/src/app/app.module.ts
@@ -4,14 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from '../infrastructure/app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { RegistrationComponent } from './registration/registration.component';
-import { FormsModule, NgModel, ReactiveFormsModule } from '@angular/forms';
-import { MaterialModule } from 'src/infrastructure/material.module';
-
-
 import { MaterialModule } from 'src/infrastructure/material.module';
 import { FormsModule, NgModel, ReactiveFormsModule } from '@angular/forms';
+
 import { LoginComponent } from './components/login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
 
 @NgModule({
   declarations: [
