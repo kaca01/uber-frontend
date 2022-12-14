55d7c0de71be7fc30e92fff9879e7e8cd69dec69
kaca01 <96604086+kaca01@users.noreply.github.com>
2022-12-14 12:39:23 +0100

    Finished ride details

--- Uber/src/app/components/history/details/passenger-details/passenger-details.component.ts
+++ Uber/src/app/components/history/details/passenger-details/passenger-details.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-passenger-details',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./passenger-details.component.css']
 })
 export class PassengerDetailsComponent {
-
+  constructor(private router : Router) {}
+  navigateBack() {
+    this.router.navigate(['passenger-history'])
+  }
 }
