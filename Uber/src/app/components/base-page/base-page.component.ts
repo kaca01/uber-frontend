1c8d40963f7387d9f3ed22e6d20d04bb088a8b74
kaca01 <96604086+kaca01@users.noreply.github.com>
2022-12-24 00:31:52 +0100

    Refactoring
    But this code is still not clean...

--- Uber/src/app/components/base-page/base-page.component.ts
+++ Uber/src/app/components/base-page/base-page.component.ts
@@ -10,76 +10,80 @@ export class BasePageComponent {
   constructor(private router : Router) {}
 
   ngOnInit() : void {
+	// getting all dialogs
     const home = document.getElementById('home');
     const history = document.getElementById('history');
     const details = document.getElementById('details');
     const ratings = document.getElementById('ride-ratings');
 
+	// navigation
     const navHome = document.getElementById('nav-home');
     const navHistory = document.getElementById('nav-history');
 
-    if (history != null && home != null && details != null && ratings != null && navHistory != null && navHome != null) {
-      history.style.display = 'none';
-      details.style.display = 'none';
-      ratings.style.display = 'none';
+	// other elements
+	const histories = document.getElementsByClassName("list-item");
+	const arr = Array.from(histories);
 
-      navHome.addEventListener('click', function click(event){
-        home.style.display = 'block';
-        history.style.display = 'none';
-        details.style.display = 'none';
-        ratings.style.display = 'none';
-      });
+	const viewRatings = document.getElementById('ratings');
+	const backToHistory = document.getElementById('back-to-history');
+	const backToDetails = document.getElementById('back-to-details');
 
-      navHistory.addEventListener('click', function click(event){
-        home.style.display = 'none';
-        history.style.display = 'block';
-        details.style.display = 'none';
-        ratings.style.display = 'none';
-      });
+    if (history && home && details && ratings && navHistory && navHome != null) {
+		// adding click listeners
+      	display("home");
 
-      const histories = document.getElementsByClassName("list-item");
-      const arr = Array.from(histories);
-      
-      arr.forEach (oneHistory =>{
-        if (oneHistory != null) {
-          oneHistory.addEventListener('click', function click(event){
-            home.style.display = 'none';
-            history.style.display = 'none';
-            details.style.display = 'block';
-            ratings.style.display = 'none';
-          });
-        }
-      });
+		navHome.addEventListener('click', function click(event){
+			display("home");
+		});
 
-      const viewRatings = document.getElementById('ratings');
-      if (viewRatings != null) {
-        viewRatings.addEventListener('click', function click(event){
-          home.style.display = 'none';
-          history.style.display = 'none';
-          details.style.display = 'none';
-          ratings.style.display = 'block';
-        });
-      }
-      
-      const backToHistory = document.getElementById('back-to-history');
-      if (backToHistory != null) {
-        backToHistory.addEventListener('click', function click(event){
-          home.style.display = 'none';
-          history.style.display = 'block';
-          details.style.display = 'none';
-          ratings.style.display = 'none';
-        });
-      }
+		navHistory.addEventListener('click', function click(event){
+			display("history");
+		});
 
-      const backToDetails = document.getElementById('back-to-details');
-      if (backToDetails != null) {
-        backToDetails.addEventListener('click', function click(event){
-          home.style.display = 'none';
-          history.style.display = 'none';
-          details.style.display = 'block';
-          ratings.style.display = 'none';
-        });
-      }
+		arr.forEach (oneHistory =>{
+			if (oneHistory != null) {
+			oneHistory.addEventListener('click', function click(event){
+				display("details");
+			});
+			}
+		});
+
+		if (viewRatings != null) {
+			viewRatings.addEventListener('click', function click(event){
+			display("ratings");
+			});
+		}
+		
+		if (backToHistory != null) {
+			backToHistory.addEventListener('click', function click(event){
+			display("history");
+			});
+		}
+
+		if (backToDetails != null) {
+			backToDetails.addEventListener('click', function click(event){
+			display("details");
+			});
+		}
     }
-  }
+
+	function display(dialog : String) : void {
+		closeAllDialogs();
+		if (home && history && details && ratings != null) {
+			if (dialog == "home") home.style.display = 'block';
+			else if (dialog == "history") history.style.display = 'block';
+			else if (dialog == "details") details.style.display = 'block';
+			else if (dialog == 'ratings') ratings.style.display = 'block';
+		}
+	}
+
+	function closeAllDialogs() : void {
+		if (home && history && details && ratings != null) {
+			home.style.display = 'none';
+			history.style.display = 'none';
+			details.style.display = 'none';
+			ratings.style.display = 'none';
+		}
+	}
+	}
 }
