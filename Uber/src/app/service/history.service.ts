fa05103bafa5fa3050339369b50786b25606c4f0
kaca01 <96604086+kaca01@users.noreply.github.com>
2023-01-06 04:56:44 +0100

    Refactoring

--- Uber/src/app/service/history.service.ts
+++ Uber/src/app/service/history.service.ts
@@ -9,7 +9,7 @@ import { AllRides } from '../components/history/ride-history/ride-history.compon
 })
 export class HistoryService {
   // TODO : after implementation of login, change the next line
-  private currentUserId : Number = 1;
+  private userId : Number = 1;
   public selectedRide : number = -1;
 
   private messageSource = new BehaviorSubject<number>(this.selectedRide);
@@ -18,7 +18,7 @@ export class HistoryService {
   constructor(private http : HttpClient) { }
 
   getHistory() : Observable<AllRides> {
-    return this.http.get<AllRides>(environment.apiHost + "api/user/" + this.currentUserId.toString() + "/ride?page=1&size=10&sort='DATE'&from='bla'&to='bla'");
+    return this.http.get<AllRides>(environment.apiHost + "api/user/" + this.userId.toString() + "/ride?page=1&size=10&sort='DATE'&from='bla'&to='bla'");
   }
 
   sendMessage(message: number) {
