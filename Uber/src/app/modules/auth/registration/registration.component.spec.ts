66187acb8d407cc4e53059f67f923b724153be38
Hristina Adamovic <hristinacina@gmail.com>
2023-02-07 16:30:14 +0100

    added registration test cases

--- Uber/src/app/modules/auth/registration/registration.component.spec.ts
+++ Uber/src/app/modules/auth/registration/registration.component.spec.ts
@@ -39,4 +39,93 @@ describe('RegistrationComponent', () => {
     element.click();
     expect(component.reg).toHaveBeenCalledTimes(0);
   });
+
+  it('form should be valid', () => {
+    component.registrationForm.controls['name'].setValue('Nina');
+    component.registrationForm.controls['surname'].setValue('Milic');
+    component.registrationForm.controls['address'].setValue('Vojvode Stepe 23');
+    component.registrationForm.controls['email'].setValue('nina@gmail.com');
+    component.registrationForm.controls['password'].setValue('123');
+    component.registrationForm.controls['repeatPassword'].setValue('123');
+    component.registrationForm.controls['telephoneNumber'].setValue('067332541');
+    expect(component.registrationForm.valid).toBeTruthy();
+  });
+
+
+  it('form should not be valid - empty fields', () => {
+    component.registrationForm.controls['name'].setValue('');
+    component.registrationForm.controls['surname'].setValue('');
+    component.registrationForm.controls['address'].setValue('');
+    component.registrationForm.controls['email'].setValue('');
+    component.registrationForm.controls['password'].setValue('');
+    component.registrationForm.controls['repeatPassword'].setValue('');
+    component.registrationForm.controls['telephoneNumber'].setValue('');
+    expect(component.registrationForm.valid).toBeFalsy();
+  });
+
+  it('form should not be valid - incorrect email format', () => {
+    component.registrationForm.controls['email'].setValue('ana');
+    component.registrationForm.controls['name'].setValue('Nina');
+    component.registrationForm.controls['surname'].setValue('Milic');
+    component.registrationForm.controls['address'].setValue('Vojvode Stepe 23');
+    component.registrationForm.controls['password'].setValue('123');
+    component.registrationForm.controls['repeatPassword'].setValue('123');
+    component.registrationForm.controls['telephoneNumber'].setValue('067332541');
+    expect(component.registrationForm.valid).toBeFalsy();
+  });
+
+  it('form should not be valid - incorrect telephone number format', () => {
+    component.registrationForm.controls['name'].setValue('Nina');
+    component.registrationForm.controls['surname'].setValue('Milic');
+    component.registrationForm.controls['address'].setValue('Vojvode Stepe 23');
+    component.registrationForm.controls['email'].setValue('nina@gmail.com');
+    component.registrationForm.controls['password'].setValue('123');
+    component.registrationForm.controls['repeatPassword'].setValue('123');
+    component.registrationForm.controls['telephoneNumber'].setValue('neka_slova');
+    expect(component.registrationForm.valid).toBeFalsy();
+  });
+
+  it('form should not be valid - incorrect name format', () => {
+    component.registrationForm.controls['name'].setValue('Nina888');
+    component.registrationForm.controls['surname'].setValue('Milic');
+    component.registrationForm.controls['address'].setValue('Vojvode Stepe 23');
+    component.registrationForm.controls['email'].setValue('nina@gmail.com');
+    component.registrationForm.controls['password'].setValue('123');
+    component.registrationForm.controls['repeatPassword'].setValue('123');
+    component.registrationForm.controls['telephoneNumber'].setValue('067332541');
+    expect(component.registrationForm.valid).toBeFalsy();
+  });
+
+  it('form should not be valid - incorrect surname format', () => {
+    component.registrationForm.controls['name'].setValue('Nina');
+    component.registrationForm.controls['surname'].setValue('Milic888');
+    component.registrationForm.controls['address'].setValue('Vojvode Stepe 23');
+    component.registrationForm.controls['email'].setValue('nina@gmail.com');
+    component.registrationForm.controls['password'].setValue('123');
+    component.registrationForm.controls['repeatPassword'].setValue('123');
+    component.registrationForm.controls['telephoneNumber'].setValue('067332541');
+    expect(component.registrationForm.valid).toBeFalsy();
+  });
+
+  it('form should not be valid - name does not have enough characters', () => {
+    component.registrationForm.controls['name'].setValue('N');
+    component.registrationForm.controls['surname'].setValue('Milic');
+    component.registrationForm.controls['address'].setValue('Vojvode Stepe 23');
+    component.registrationForm.controls['email'].setValue('nina@gmail.com');
+    component.registrationForm.controls['password'].setValue('123');
+    component.registrationForm.controls['repeatPassword'].setValue('123');
+    component.registrationForm.controls['telephoneNumber'].setValue('067332541');
+    expect(component.registrationForm.valid).toBeFalsy();
+  });
+
+  it('form should not be valid - surname does not have enough characters', () => {
+    component.registrationForm.controls['name'].setValue('Nina');
+    component.registrationForm.controls['surname'].setValue('M');
+    component.registrationForm.controls['address'].setValue('Vojvode Stepe 23');
+    component.registrationForm.controls['email'].setValue('nina@gmail.com');
+    component.registrationForm.controls['password'].setValue('123');
+    component.registrationForm.controls['repeatPassword'].setValue('123');
+    component.registrationForm.controls['telephoneNumber'].setValue('067332541');
+    expect(component.registrationForm.valid).toBeFalsy();
+  });
 });
