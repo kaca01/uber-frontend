89e527eb421251f94297ce2f77916b6d0cf8a5f2
kaca01 <96604086+kaca01@users.noreply.github.com>
2023-02-07 13:09:01 +0100

    Refactoring

--- Uber/src/app/modules/account/options/options.component.spec.ts
+++ Uber/src/app/modules/account/options/options.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { AccountPassengerComponent } from './options.component';
+import { OptionsComponent } from './options.component';
 
-describe('AccountPassengerComponent', () => {
-  let component: AccountPassengerComponent;
-  let fixture: ComponentFixture<AccountPassengerComponent>;
+describe('OptionsComponent', () => {
+  let component: OptionsComponent;
+  let fixture: ComponentFixture<OptionsComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ AccountPassengerComponent ]
+      declarations: [ OptionsComponent ]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(AccountPassengerComponent);
+    fixture = TestBed.createComponent(OptionsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
