fde5a5d22e554f02a4129ad593cd4422dee4a37c
savic-a <anastasijas557@gmail.com>
2023-02-07 13:39:57 +0100

    Start testing registration

--- Uber/src/app/modules/auth/registration/registration.component.spec.ts
+++ Uber/src/app/modules/auth/registration/registration.component.spec.ts
@@ -1,23 +1,42 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
 
 import { RegistrationComponent } from './registration.component';
 
 describe('RegistrationComponent', () => {
   let component: RegistrationComponent;
   let fixture: ComponentFixture<RegistrationComponent>;
+  let debug: DebugElement;
+  let element: HTMLElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ RegistrationComponent ]
+      declarations: [ RegistrationComponent ],
+      imports: [
+        FormsModule,
+        ReactiveFormsModule
+      ]
     })
     .compileComponents();
+  });
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(RegistrationComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    debug = fixture.debugElement.query(By.css('form'));
+    element = debug.nativeElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call the onSubmit method', () => {
+    spyOn(component, "reg");
+    element = fixture.debugElement.query(By.css('reg-btn')).nativeElement;
+    element.click();
+    expect(component.reg).toHaveBeenCalledTimes(0);
   });
 });
